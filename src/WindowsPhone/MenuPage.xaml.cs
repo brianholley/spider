9d21b9edc2ffbe48b88df5af98739f04081d7436
Brian Holley <brian.holley@hotmail.com>
2015-04-25 12:18:26 -0700

    Code and assets for menu refactor

---
+++ src/WindowsPhone/MenuPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Spider
+{
+	public partial class MenuPage : PhoneApplicationPage
+	{
+		public MenuPage()
+		{
+			InitializeComponent();
+		}
+
+		private void NewGameClickOneSuit(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void NewGameClickTwoSuit(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void NewGameClickFourSuit(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OptionsButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void StatisticsButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void AboutButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
