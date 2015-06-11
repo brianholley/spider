92e43df4262b24751d0fb9ff4f7a1cd8f3b0ab8b
Brian Holley <brian.holley@hotmail.com>
2015-06-10 21:17:36 -0700

    Version bumps and such for v1.3 release

--- src/WindowsPhone/Properties/AssemblyInfo.cs
+++ src/WindowsPhone/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsPhone")]
-[assembly: AssemblyProduct("WindowsPhone")]
+[assembly: AssemblyTitle("Spider.WP")]
+[assembly: AssemblyProduct("Spider.WP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0426.0")]
-[assembly: AssemblyFileVersion("2.0.0426.0")]
+[assembly: AssemblyVersion("1.3.0610.0")]
+[assembly: AssemblyFileVersion("1.3.0610.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
